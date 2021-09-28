cad2052209082c2c16be1bba325173a4fa86c125
Lemuel Canady <lem.canady@gmail.com>
2021-09-27 20:09:02 -0700

    changing config

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,17 +1,7 @@
 import { readFileSync } from "fs";
-import glob from "glob";
+
 import { basename } from "path/posix";
 
 interface IConfig {
   [key: string]: any;
 }
-
-const config: IConfig = {};
-const files = glob.sync("config/**/*.json");
-files.forEach((file) => {
-  config[basename(file).split(".")[0]] = JSON.parse(
-    readFileSync(file, "utf-8")
-  );
-});
-
-export { config };
